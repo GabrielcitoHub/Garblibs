aeac45f7e67baeefb119dcb2f1d1bc34ca2b3776
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-12-03 13:57:51 -0300

    actually fixed it now! (maybe... i hope)

--- sprite/objects/layer.lua
+++ sprite/objects/layer.lua
@@ -2,24 +2,26 @@ return function(tag, order, sprites, extra)
     ---@type Layer
     local self = {}
     extra = extra or {}
+    local manager = extra.manager
+    local cwd = manager.cwd
     
     function self:load()
-        require(extra.cwd .. "layer.toggleables")(self)
-        require(extra.cwd .. "layer.functions")(self)
-        return require(extra.cwd .. "layer.load")(self, tag, order, sprites)
+        require(cwd .. "layer.toggleables")(self)
+        require(cwd .. "layer.functions")(self)
+        return require(cwd .. "layer.load")(self, tag, order, sprites)
     end
 
     function self:update(dt)
         if not self.deltatime then dt = 1 end
-        return require(extra.cwd .. "layer.update")(self, dt)
+        return require(cwd .. "layer.update")(self, dt)
     end
 
     function self:draw()
-        return require(extra.cwd .. "layer.draw")(self, extra.manager)
+        return require(cwd .. "layer.draw")(self, manager)
     end
 
     function self:remove()
-        return require(extra.cwd .. "layer.remove")(self, extra.manager)
+        return require(cwd .. "layer.remove")(self, manager)
     end
 
     return self
--- sprite/objects/sprite.lua
+++ sprite/objects/sprite.lua
@@ -2,24 +2,26 @@ return function(tag, path, x, y, extra)
     ---@type Sprite
     local self = {}
     extra = extra or {}
+    local manager = extra.manager
+    local cwd = manager.cwd
 
     function self:load()
-        require(extra.cwd .. "sprite.toggleables")(self)
-        require(extra.cwd .. "sprite.functions")(self)
-        return require(extra.cwd .. "sprite.load")(self, tag, path, x, y)
+        require(cwd .. "sprite.toggleables")(self)
+        require(cwd .. "sprite.functions")(self)
+        return require(cwd .. "sprite.load")(self, tag, path, x, y)
     end
 
     function self:update(dt)
         if not self.deltatime then dt = 1 end
-        return require(extra.cwd .. "sprite.update")(self, dt)
+        return require(cwd .. "sprite.update")(self, dt)
     end
 
     function self:draw()
-        return require(extra.cwd .. "sprite.draw")(self)
+        return require(cwd .. "sprite.draw")(self)
     end
 
     function self:remove()
-        return require(extra.cwd .. "sprite.remove")(self, extra.manager)
+        return require(cwd .. "sprite.remove")(self, manager)
     end
 
     return self
